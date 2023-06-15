3bfacdc71b7bdb0504206144f679ed4ef8aceb95
Tarun Garg <44539397+Tarungarg98@users.noreply.github.com>
2023-06-16 02:16:59 +0530

    Added GA4, changed page name

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Layout from "./components/Layout/layout";
 import { AppProvider, AppContext } from "./context/AppContext";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-LN7ZYBXKG7");
 const App = () => {
   return (
     <AppProvider>
