27e7fc0fd10552ab339eddf6442a5d500f800868
Tarun Garg <44539397+Tarungarg98@users.noreply.github.com>
2023-07-01 09:21:56 +0530

    Added styling to intro page

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -6,6 +6,7 @@ import email_img from "./images/email.png";
 import linkedin_img from "./images/linkedin.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./intro.css";
+import { TypewriterEffect, DelayedContent } from "../utils/utils";
 
 import { AppContext } from "../../context/AppContext";
 
@@ -21,55 +22,60 @@ const IntroPage = () => {
     <div>
       <div className="intro_page" id="intro_page">
         <div className="brief">
-          <h2 className="myname">Hi, I'm Tarun Garg</h2>
-          <h3>Data Scientist @ Cars24</h3>
-          Welcome to my personal website! I'm a passionate data scientist with
-          expertise in leveraging data-driven insights to solve complex business
-          challenges. With experience in natural language processing, anomaly
-          detection, and fraud modeling, I excel at applying advanced machine
-          learning techniques to extract valuable insights from data. Throughout
-          my career, I have worked with renowned organizations, collaborating
-          with exceptional mentors and contributing to impactful projects. I am
-          driven by a passion for continuous learning and a desire to make a
-          positive impact through innovative data-driven solutions. Feel free to
-          explore my website to learn more about my projects, expertise, and
-          achievements. I'm open to new opportunities and collaborations, so
-          let's connect and create something extraordinary together in the world
-          of data science.
-          <br></br>
-          {/* <button class="blog_url" onClick={changeView}>
+          <div className="myname">
+            <h1>
+              <TypewriterEffect text={"Hi, I'm Tarun Garg"} speed={100} />
+            </h1>
+            <h2>
+              <TypewriterEffect
+                text={"Data Scientist @ Cars24"}
+                speed={50}
+                startDelay={2000}
+              />
+            </h2>
+          </div>
+          <div className="about_me">
+            <TypewriterEffect
+              speed={10}
+              startDelay={4000}
+              text={
+                "Welcome to my personal website! I'm a passionate data scientist with expertise in leveraging data-driven insights to solve complex business challenges. With experience in natural language processing, anomaly detection, and fraud modeling, I excel at applying advanced machine learning techniques to extract valuable insights from data.\n Throughout my career, I have worked with renowned organizations, collaborating with exceptional mentors and contributing to impactful projects. I am driven by a passion for continuous learning and a desire to make a positive impact through innovative data-driven solutions. Feel free to explore my website to learn more about my projects, expertise, and achievements. I'm open to new opportunities and collaborations, so let's connect and create something extraordinary together in the world of data science."
+              }
+            />
+          </div>
+        </div>
+        {/* <button class="blog_url" onClick={changeView}>
           Click here to check out my Data Science Journey
         </button> */}
-          <div className="icon">
-            <a
-              href="https://drive.google.com/file/d/1Mdv1OSIuPaHuFCK637n77w1bpdYWZIVG/view"
-              target="_blank"
-              title="Resume"
-            >
-              <img src={resume_img} alt="Resume"></img>
-            </a>
-            <a
-              href="https://github.com/Tarungarg98"
-              target="_blank"
-              title="Github"
-            >
-              <img src={github_img} alt="Github"></img>
-            </a>
-            <a
-              href="https://mail.google.com/mail/?view=cm&fs=1&to=gargtarun13@gmail.com"
-              title="Mail"
-              target="_blank"
-            >
-              <img src={email_img}></img>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/tarungarg98/"
-              title="LinkedIn"
-              target="_blank"
-            >
-              <img src={linkedin_img} alt="Linkedin"></img>
-            </a>
-          </div>
+        <div className="icon">
+          <a
+            href="https://drive.google.com/file/d/1Mdv1OSIuPaHuFCK637n77w1bpdYWZIVG/view"
+            target="_blank"
+            title="Resume"
+          >
+            <img src={resume_img} alt="Resume"></img>
+          </a>
+          <a
+            href="https://github.com/Tarungarg98"
+            target="_blank"
+            title="Github"
+          >
+            <img src={github_img} alt="Github"></img>
+          </a>
+          <a
+            href="https://mail.google.com/mail/?view=cm&fs=1&to=gargtarun13@gmail.com"
+            title="Mail"
+            target="_blank"
+          >
+            <img src={email_img}></img>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/tarungarg98/"
+            title="LinkedIn"
+            target="_blank"
+          >
+            <img src={linkedin_img} alt="Linkedin"></img>
+          </a>
         </div>
       </div>
       <br></br>
