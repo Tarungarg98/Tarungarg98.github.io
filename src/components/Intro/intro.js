e0a46390581382b66bd404092c507e90e8b4c730
Tarun Garg <44539397+Tarungarg98@users.noreply.github.com>
2023-07-02 07:47:06 +0530

    skills animation

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -36,7 +36,7 @@ const IntroPage = () => {
           </div>
           <div className="about_me">
             <TypewriterEffect
-              speed={10}
+              speed={1}
               startDelay={4000}
               text={
                 "Welcome to my personal website! I'm a passionate data scientist with expertise in leveraging data-driven insights to solve complex business challenges. With experience in natural language processing, anomaly detection, and fraud modeling, I excel at applying advanced machine learning techniques to extract valuable insights from data.\n Throughout my career, I have worked with renowned organizations, collaborating with exceptional mentors and contributing to impactful projects. I am driven by a passion for continuous learning and a desire to make a positive impact through innovative data-driven solutions. Feel free to explore my website to learn more about my projects, expertise, and achievements. I'm open to new opportunities and collaborations, so let's connect and create something extraordinary together in the world of data science."
