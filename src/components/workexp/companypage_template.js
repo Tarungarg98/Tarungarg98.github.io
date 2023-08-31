0f9b1ca6884c6aec97aba8006fbe386f38a3ad60
Tarun Garg <44539397+Tarungarg98@users.noreply.github.com>
2023-08-31 20:30:49 +0530

    update

--- src/components/workexp/companypage_template.js
+++ src/components/workexp/companypage_template.js
@@ -4,34 +4,46 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
 import accordion_styles from "./accordion_styles.module.css";
 import chevronDown from "./chevron-down.svg";
+import ReactGA from "react-ga4";
 /**
  * @type {React.ExoticComponent<import('@szhsin/react-accordion').AccordionItemProps>}
  */
-const AccordionItem = ({ header, ...rest }) => (
-  <Item
-    {...rest}
-    header={
-      <>
-        {header}
-        <img
-          className={accordion_styles.chevron}
-          src={chevronDown}
-          alt="Chevron Down"
-        />
-      </>
-    }
-    className={accordion_styles.item}
-    buttonProps={{
-      className: ({ isEnter }) =>
-        `${accordion_styles.itemBtn} ${
-          isEnter && accordion_styles.itemBtnExpanded
-        }`,
-    }}
-    contentProps={{ className: accordion_styles.itemContent }}
-    panelProps={{ className: accordion_styles.itemPanel }}
-  />
-);
 
+const AccordionItem = ({ header, ...rest }) => {
+  const handleAccordionItemClick = () => {
+    // Track the event when the accordion item is clicked
+    ReactGA.event("project-title-click", {
+      category: "project-title",
+      label: header, // Use the 'header' prop as the 'project_name'
+    });
+  };
+
+  return (
+    <Item
+      {...rest}
+      header={
+        <>
+          {header}
+          <img
+            className={accordion_styles.chevron}
+            src={chevronDown}
+            alt="Chevron Down"
+          />
+        </>
+      }
+      className={accordion_styles.item}
+      buttonProps={{
+        className: ({ isEnter }) =>
+          `${accordion_styles.itemBtn} ${
+            isEnter && accordion_styles.itemBtnExpanded
+          }`,
+        onClick: handleAccordionItemClick, // Call the GA4 tracking function on click
+      }}
+      contentProps={{ className: accordion_styles.itemContent }}
+      panelProps={{ className: accordion_styles.itemPanel }}
+    />
+  );
+};
 const CompanyPage = (props) => {
   return (
     <div className="companypage">
